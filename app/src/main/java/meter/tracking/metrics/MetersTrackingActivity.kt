be79679398a6ab7286e27a62336c99fd98d24d02
Thierry Weissbeck <thierry.weissbeck@lyra-network.com>
2018-05-09 16:24:22 +0200

    Rename layout file

--- app/src/main/java/meter/tracking/metrics/MetersTrackingActivity.kt
+++ app/src/main/java/meter/tracking/metrics/MetersTrackingActivity.kt
@@ -2,7 +2,8 @@ package meter.tracking.metrics
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_metrics_tracker_list.*
 import meter.tracking.R
 import meter.tracking.db.DataBase
 import meter.tracking.metrics.view.MetricAdapter
@@ -15,19 +16,22 @@ import meter.tracking.metrics.view.MetricAdapter
 class MetersTrackingActivity : AppCompatActivity() {
 
     private lateinit var metricAdapter: MetricAdapter
+    private lateinit var layoutManager: LinearLayoutManager
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_metrics_tracker_list)
 
         val db = DataBase.getInstance(applicationContext)
         val repository = MetricRepository(db.metricDao)
         val metrics = repository.getMetrics()
         this.metricAdapter = MetricAdapter(metrics)
+        this.layoutManager = LinearLayoutManager(this)
 
         list.apply {
-
+            layoutManager = MetersTrackingActivity@ layoutManager
+            adapter = metricAdapter
         }
     }
 }
