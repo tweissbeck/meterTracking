2abf72945eed1f2a7f0fe058ac34caab9f1fa0ec
Thierry Weissbeck <thierry.weissbeck@lyra-network.com>
2018-05-09 16:24:37 +0200

    Add test data

--- app/src/main/java/meter/tracking/metrics/MetricRepository.kt
+++ app/src/main/java/meter/tracking/metrics/MetricRepository.kt
@@ -1,6 +1,7 @@
 package meter.tracking.metrics
 
 import meter.tracking.db.dao.MetricDao
+import meter.tracking.db.model.HistoryFrequency
 import meter.tracking.db.model.Metric
 import meter.tracking.db.model.MetricsWithRecord
 
@@ -15,7 +16,16 @@ class MetricRepository(private val metricDao: MetricDao) : MetricDataSource {
 
     override fun getMetrics(): List<Metric> {
         return if (metrics == null) {
-            this.metrics = metricDao.getAll()
+            this.metrics = metricDao.getAll() + listOf(Metric(1, "CAR1", 1420, "Km", HistoryFrequency.MONTHLY),
+                                                       Metric(2, "CAR2", 15874, "Km", HistoryFrequency.MONTHLY),
+                                                       Metric(3, "Water", 1420, "L", HistoryFrequency.MONTHLY),
+                                                       Metric(4, "Elec", 142078, "Watt", HistoryFrequency.MONTHLY),
+                                                       Metric(5, "CAR1", 1420, "Km", HistoryFrequency.MONTHLY),
+                                                       Metric(6, "CAR1", 1420, "Km", HistoryFrequency.MONTHLY),
+                                                       Metric(8, "CAR1", 1420, "Km", HistoryFrequency.MONTHLY),
+                                                       Metric(9, "CAR1", 1420, "Km", HistoryFrequency.MONTHLY),
+                                                       Metric(10, "CAR1", 1420, "Km", HistoryFrequency.MONTHLY),
+                                                       Metric(11, "CAR1", 1420, "Km", HistoryFrequency.MONTHLY))
             this.metrics!!
         } else {
             this.metrics!!
