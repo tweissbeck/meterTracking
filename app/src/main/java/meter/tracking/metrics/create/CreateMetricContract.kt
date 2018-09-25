b2a68d25aad3cb556bfcf2f457d11097aaed34d0
Thierry Weissbeck <thierry.weissbeck@gmail.com>
2018-09-25 17:17:40 +0200

    Add MVP contract for CreateMetricActivity

---
+++ app/src/main/java/meter/tracking/metrics/create/CreateMetricContract.kt
@@ -0,0 +1,14 @@
+package meter.tracking.metrics.create
+
+import meter.tracking.BaseView
+
+/**
+ * @author tweissbeck
+ * @since 1.0.0
+ */
+interface CreateMetricContract {
+
+    interface View : BaseView<Presenter>
+
+    interface Presenter
+}
