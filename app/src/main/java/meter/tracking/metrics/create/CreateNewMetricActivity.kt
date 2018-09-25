80434d9e544bb98ad5f62af00320db2b447e5d6a
Thierry Weissbeck <thierry.weissbeck@lyra-network.com>
2018-09-25 17:10:22 +0200

    Add action in menu into the create metric activity (WIP)

--- app/src/main/java/meter/tracking/metrics/create/CreateNewMetricActivity.kt
+++ app/src/main/java/meter/tracking/metrics/create/CreateNewMetricActivity.kt
@@ -2,6 +2,9 @@ package meter.tracking.metrics.create
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 import meter.tracking.R
 
 /**
@@ -12,5 +15,21 @@ class CreateNewMetricActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_new_metric)
+
+        val actionBar = findViewById<Toolbar>(R.id.create_metric_activity_toolbar)
+        setSupportActionBar(actionBar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.create_metric_activity_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId) {
+        R.id.action_create_metric_cancel -> {
+            onBackPressed()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
