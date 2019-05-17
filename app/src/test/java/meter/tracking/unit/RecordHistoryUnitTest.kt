ed5f61764b74c8d70fc4ecd43808c72e54abc249
tweissbeck <thierry.weissbeck@glyra-network.com>
2019-05-17 09:27:02 +0200

    Fix unit test of ReverseLocalDateRange Still in progress: adding more tests soon(tm)

--- app/src/test/java/meter/tracking/unit/RecordHistoryUnitTest.kt
+++ app/src/test/java/meter/tracking/unit/RecordHistoryUnitTest.kt
@@ -1,16 +1,14 @@
 package meter.tracking.unit
 
-import org.junit.Assert.assertEquals
-import meter.tracking.db.model.HistoryFrequency
-import meter.tracking.db.model.MetricRecord
-import meter.tracking.db.model.MetricsWithRecord
-import meter.tracking.metrics.detail.history.RecordHistory
+import meter.tracking.metrics.detail.history.ReverseLocalDateRange
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
 /**
- * Unit testing of [meter.tracking.metrics.detail.history.RecordHistory]
+ * Unit testing of [meter.tracking.metrics.detail.history.ReverseLocalDateRange]
  * @author tweissbeck
  * @since 1.0.0
  */
@@ -25,57 +23,18 @@ class RecordHistoryUnitTest {
     }
 
     @Test
-    fun testEmpty() {
-        val metricWithRecord = buildMetric(HistoryFrequency.DAILY, LocalDate.now())
-        metricWithRecord.records = listOf(buildRecord(LocalDate.now(), 12))
-        val history = RecordHistory(metricWithRecord).history
-        assertEquals(1, history.size)
+    fun testFewDay() {
+        val dateRange = ReverseLocalDateRange(ChronoUnit.DAYS, LocalDate.now().minusDays(1), LocalDate.now())
+
+        val now = LocalDate.now()
+        assertTrue(dateRange.contains(now))
+        assertTrue(dateRange.contains(now.minusDays(1)))
+        assertFalse(dateRange.contains(now.minusDays(2)))
+        assertFalse(dateRange.contains(now.plusDays(1)))
+
+        val iterator = dateRange.iterator()
+        assertEquals(now, iterator.next())
+        assertEquals(now.minusDays(1), iterator.next())
     }
 
-    @Test
-    fun testDays() {
-        val metricWithRecord = buildMetric(HistoryFrequency.DAILY, LocalDate.now().minusDays(11))
-        metricWithRecord.records = listOf(
-                buildRecord(LocalDate.now(), 1),
-                buildRecord(LocalDate.now().minusDays(2), 2),
-                buildRecord(LocalDate.now().minusDays(4), 4),
-                buildRecord(LocalDate.now().minusDays(5), 5),
-                buildRecord(LocalDate.now().minusDays(10), 10),
-                buildRecord(LocalDate.now().minusDays(11), 11)
-        )
-        val history = RecordHistory(metricWithRecord).history
-        assertEquals(12, history.size)
-        val iterator = history.iterator()
-        var historyDate = LocalDate.now()
-
-        // Check date & order
-        iterator.forEach {
-            assertEquals(historyDate, it.localDate)
-            historyDate = historyDate.minusDays(1)
-        }
-    }
-
-    @Test
-    fun testMonth() {
-        val metricsWithRecord = buildMetric(HistoryFrequency.MONTHLY, LocalDate.now().minusMonths(2))
-        metricsWithRecord.records = listOf(
-                buildRecord(LocalDate.now().minusMonths(2).withDayOfMonth(1), 30)
-        )
-        val history = RecordHistory(metricsWithRecord).history
-        assertEquals(3, history.size)
-        val iterator = history.iterator()
-        var historyDate = LocalDate.now().withDayOfMonth(1)
-        iterator.forEach {
-            assertEquals(historyDate, it.localDate)
-            historyDate = historyDate.minusMonths(1)
-        }
-    }
-
-    private fun buildMetric(frequency: HistoryFrequency, startDate: LocalDate): MetricsWithRecord {
-        return MetricsWithRecord("abc", metricId++, "MS", frequency, startDate)
-    }
-
-    private fun buildRecord(date: LocalDate, value: Long, metricId: Long? = null): MetricRecord {
-        return MetricRecord(recordId++, metricId ?: this.metricId - 1, value, date)
-    }
 }
