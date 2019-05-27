58f4d170e885c13ed787b02b3c260d9be6012a69
tweissbeck <thierry.weissbeck@glyra-network.com>
2019-05-27 08:58:57 +0200

    Remove unused class attribute

--- app/src/main/java/meter/tracking/di/ApplicationModule.kt
+++ app/src/main/java/meter/tracking/di/ApplicationModule.kt
@@ -7,13 +7,12 @@ import meter.tracking.db.MetricsDataBase
 import meter.tracking.db.dao.MetricDao
 import meter.tracking.datasource.MetricDataSource
 import meter.tracking.datasource.MetricRepository
-import meter.tracking.db.dao.MeasuringTypeDao
-import meter.tracking.db.model.MeasuringType
 import meter.tracking.rx.AndroidSchedulerProvider
 import meter.tracking.rx.SchedulerProvider
 import org.koin.dsl.module.module
 
 /**
+ * Define KOIN module
  * @author tweissbeck
  * @since 1.0.0
  */
@@ -22,7 +21,7 @@ object ApplicationModule {
         single { Room.databaseBuilder(get(), MetricsDataBase::class.java, "metric_base").build() }
         single<MetricDataSource> { MetricRepository(getMetricDAO(get())) }
         single<MeasuringTypeDataSource> { getMeasuringType(get()) }
-        single<SchedulerProvider> { AndroidSchedulerProvider(get()) }
+        single<SchedulerProvider> { AndroidSchedulerProvider() }
     }
 
     private fun getMetricDAO(db: MetricsDataBase): MetricDao = db.metricDao
--- app/src/main/java/meter/tracking/rx/AndroidSchedulerProvider.kt
+++ app/src/main/java/meter/tracking/rx/AndroidSchedulerProvider.kt
@@ -1,6 +1,5 @@
 package meter.tracking.rx
 
-import android.content.Context
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -10,8 +9,14 @@ import io.reactivex.schedulers.Schedulers
  * @author tweissbeck
  * @since 1.0.0
  */
-class AndroidSchedulerProvider(context: Context): SchedulerProvider {
+class AndroidSchedulerProvider : SchedulerProvider {
+    /**
+     *  @see AndroidSchedulers.mainThread
+     */
     override fun ui(): Scheduler = AndroidSchedulers.mainThread()
 
+    /**
+     * @see Schedulers.io
+     */
     override fun io(): Scheduler = Schedulers.io()
 }
